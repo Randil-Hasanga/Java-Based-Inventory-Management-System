7ad093dc7210ce4ba09f8093c8d94804fac174db
Randil-Hasanga <randil_2020690@fot.ruh.ac.lk>
2023-10-15 17:57:44 +0530

    Created other user dashboards

---
+++ src/main/java/com/stockportfoliomanagementsystem/AccountingManager/AccountingManagerController.java
@@ -0,0 +1,15 @@
+package com.stockportfoliomanagementsystem.AccountingManager;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+
+public class AccountingManagerController{
+
+    @FXML
+    private LineChart<?, ?> lineChart;
+
+    @FXML
+    private PieChart pieChart;
+
+}
---
+++ src/main/java/com/stockportfoliomanagementsystem/HRManager/HRManagerController.java
@@ -0,0 +1,15 @@
+package com.stockportfoliomanagementsystem.HRManager;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+
+public class HRManagerController{
+
+    @FXML
+    private LineChart<?, ?> lineChart;
+
+    @FXML
+    private PieChart pieChart;
+
+}
