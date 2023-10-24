709542b4f7464a08de665f84de1dd9e5c79e69e6
Randil-Hasanga <randil_2020690@fot.ruh.ac.lk>
2023-10-24 13:12:17 +0530

    Created Common directory for common classes

---
+++ src/main/java/com/stockportfoliomanagementsystem/Common/viewStockController.java
@@ -0,0 +1,111 @@
+package com.stockportfoliomanagementsystem.Common;
+
+import com.stockportfoliomanagementsystem.MySqlCon;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class viewStockController implements Initializable {
+
+    Connection conn = MySqlCon.MysqlMethod();
+    @FXML
+    private TableView<ObservableList<String>> tblStock;
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    void manageUsers(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/stockportfoliomanagementsystem/PortfolioManager/ManageUsers.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setHeight(700);
+        stage.setWidth(1210);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    @FXML
+    void onBackButton(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/stockportfoliomanagementsystem/PortfolioManager/PortfolioManagerDashboard.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setHeight(700);
+        stage.setWidth(1210);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    @FXML
+    void onSupplierButton(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/stockportfoliomanagementsystem/PortfolioManager/viewSuppliers.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setHeight(700);
+        stage.setWidth(1210);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        ObservableList<TableColumn<ObservableList<String>, ?>> columns = tblStock.getColumns();
+        columns.clear();
+
+        // Define fixed column names
+        String[] columnNames = {"Product Id","Product Name","Price","Quantity","Product Description","Supplier ID"};
+
+        double columnWidth = (tblStock.getPrefWidth()) / (columnNames.length)-2;
+
+        // Add the columns to the TableView with fixed names
+        for (int i = 0; i < columnNames.length; i++) {
+            final int columnIndex = i;
+            TableColumn<ObservableList<String>, String> column = new TableColumn<>(columnNames[i]);
+            column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(columnIndex)));
+            column.setPrefWidth(columnWidth);
+            columns.add(column);
+        }
+
+        String sql = "SELECT P_ID, P_Name, Price, Qty, P_Description, S_ID FROM stock"; // Replace with your table name
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+            while (rs.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= columnNames.length; i++) {
+                    row.add(rs.getString(i));
+                }
+                data.add(row);
+            }
+
+            tblStock.setItems(data);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
